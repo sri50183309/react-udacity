816d69129ec1362228f313ca921c0058d4e3759c
Sriram <sriram.mahalingam@gmail.com>
2020-07-24 07:26:06 -0400

    added create/list contact module

--- reactnd-contacts-app/src/App.js
+++ reactnd-contacts-app/src/App.js
@@ -1,30 +1,25 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
+import * as ContactsAPI from  './utils/ContactsAPI'
 
 
+import CreateContact from './CreateContact'
 
 class App extends Component {
   state = {
     contacts: [
-      {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        "handle": "karen_isgrigg",
-        "avatarURL": "http://localhost:5001/karen.jpg"
-      },
-      {
-        "id": "richard",
-        "name": "Richard Kalehoff",
-        "handle": "richardkalehoff",
-        "avatarURL": "http://localhost:5001/richard.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    ],
+    screen: 'create'
+  }
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState(() =>({
+        contacts
+        
+      }))
+    }
+
+    )
   }
   removeContact = (contact) => {
     this.setState((currentState) => ({
@@ -35,16 +30,23 @@ class App extends Component {
     }
 
     )
-
     )
+    ContactsAPI.remove(contact)
+
   }
   render() {
     return (
       <div>
+        {this.state.screen === 'create' && (
+          <CreateContact/>
+        )}
+         {this.state.screen === 'list' && (
         <ListContacts 
           contacts={this.state.contacts}
           onDeleteContact={this.removeContact} />
-      </div>
+             
+             )}
+             </div>
     );
   }
 }
